c8e06c1c4f820b2c91730abc62d206abc3134d74
JayAree999 <jayyqwe123@gmail.com>
2023-02-24 05:43:41 +0700

    Add Jett movement

--- Assets/Flat Platformer Template/Scripts/PlayerMovement.cs
+++ Assets/Flat Platformer Template/Scripts/PlayerMovement.cs
@@ -1,55 +1,170 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 1.75f; // speed of player movement
-    public float jumpForce = 2.5f; // force applied when jumping
-    public float secondJumpForce = 5f; // additional force applied on second jump
-    public float secondJumpAngle = 90f; // angle at which the second jump force is applied
-    private Rigidbody2D rb; // reference to the player's rigidbody
-    private int numJumps; // number of jumps the player has performed
+    public float moveSpeed = 1.4f;
+    public float jumpForce = 2f;
 
-    // Start is called before the first frame update
+    public float fallMultiplier = 1.5f;
+    public float levitateSpeed = 2.7f;
+    public float levitateDuration = 0.1f;
+    public float levitateCooldown = 0.5f;
+
+    private bool isLevitating;
+    private float lastLevitateTime = -100f;
+
+    private Rigidbody2D rb;
+    private BoxCollider2D boxCollider;
+    private bool isGrounded;
+    private bool isDashing;
+    private bool hasJumped;
+    private float lastDashTime = -100f;
+    private float dashDistanceTraveled2 = 0f;
+
+    public float dashDistance = 1.125f;
+    public float dashCooldown = 0.5f;
+
     void Start()
     {
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
-        numJumps = 0;
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // horizontal movement
-        float moveInput = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
-
-        // jumping
-        if (Input.GetKeyDown(KeyCode.Space))
+        HandleMovement();
+        HandleFlipping();
+        CheckGrounded();
+        HandleJumping();
+        HandleInstantDash();
+        HandleFloating();
+        HandleLevitate();
+        if (isGrounded)
         {
-            if (numJumps == 0)
-            {
-                rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-            }
-            else if (numJumps == 1)
-            {
-                Vector2 jumpDirection = new Vector2(Mathf.Sign(rb.velocity.x), 1f).normalized;
-                Vector2 jumpForceVector = jumpDirection * secondJumpForce;
-                rb.velocity = new Vector2(0f, 0f);
-                rb.AddForce(jumpForceVector, ForceMode2D.Impulse);
-            }
-
-            numJumps++;
+            hasJumped = false;
         }
     }
 
-    // Reset the number of jumps when the player touches the ground
-    private void OnCollisionEnter2D(Collision2D collision)
+    void CheckGrounded()
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        isGrounded = false;
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(boxCollider.bounds.center, boxCollider.bounds.size, 0f);
+        foreach (Collider2D collider in colliders)
         {
-            numJumps = 0;
+            if (collider.gameObject != gameObject && collider.tag == "Ground")
+            {
+                isGrounded = true;
+                break;
+            }
         }
+    }
+    private Animator animator;
+    void HandleMovement()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(horizontalMovement * moveSpeed, rb.velocity.y);
+        animator.SetFloat("Horizontal", Mathf.Abs(horizontalMovement));
+    }
+
+    void HandleFloating()
+    {
+        if (Input.GetKey(KeyCode.Space) && !isGrounded && !isLevitating) {
+            rb.gravityScale = 0.5f;
+        } else {
+            rb.gravityScale = 1f;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        HandleFloating();
+        HandleLevitate();
+    }
+
+    void HandleJumping()
+    {
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            hasJumped = true;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        if (hasJumped && (Input.GetKeyUp(KeyCode.Space) || rb.velocity.y < 0))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    void HandleInstantDash()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && Time.time - lastDashTime > dashCooldown)
+        {
+            lastDashTime = Time.time;
+            dashDistanceTraveled2 = 0f;
+            Vector2 dashDirection = transform.localScale.x > 0 ? Vector2.right : Vector2.left;
+            StartCoroutine(PerformDash(dashDirection));
+        }
+    }
+
+    IEnumerator PerformDash(Vector2 direction)
+    {
+        while (dashDistanceTraveled2 < dashDistance)
+        {
+            float dashDistanceLeft = dashDistance - dashDistanceTraveled2;
+            float dashDistanceThisFrame = Mathf.Min(dashDistanceLeft, Time.deltaTime * moveSpeed * 40f);
+            rb.position += direction * dashDistanceThisFrame;
+            dashDistanceTraveled2 += dashDistanceThisFrame;
+            yield return null;
+        }
+    }
+
+
+
+    
+
+    void HandleFlipping()
+    {
+        Vector3 localScale = transform.localScale;
+
+        if (rb.velocity.x > 0)
+        {
+            localScale.x = 1;
+        }
+        else if (rb.velocity.x < 0)
+        {
+            localScale.x = -1;
+        }
+
+        transform.localScale = localScale;
+    }
+
+    void HandleLevitate()
+    {
+        if (Time.time - lastLevitateTime > levitateCooldown)
+        {
+            if (Input.GetKeyDown(KeyCode.Q) && !isDashing && !isLevitating)
+            {
+                isLevitating = true;
+                lastLevitateTime = Time.time;
+                StartCoroutine(Levitate());
+            }
+        }
+    }
+
+
+    IEnumerator Levitate()
+    {
+        float startTime = Time.time;
+        while (Time.time - startTime < levitateDuration)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, levitateSpeed);
+            yield return null;
+        }
+
+        isLevitating = false;
     }
 }
+
+
